411e9d12072bdd00e4f8138eb428285570f4d57b
johns <jsantore@bridgew.edu>
2022-02-03 17:08:45 -0500

    completed project, both ratings and top250 saved to file

--- main.py
+++ main.py
@@ -16,16 +16,43 @@ def get_top_250_data()->list[dict]:
     return show_list
 
 
-def report_results(top_show_data:list[dict]):
+def report_results(data_to_write:list[dict]):
     with open("Output.txt", mode='a') as outputFile:  # open the output file for appending
-        for show in top_show_data:
+        for show in data_to_write:
             print(show, file=outputFile)  # write each data item to file
             print("\n", file=outputFile)
             print("===================================================================", file=outputFile)
 
 
+def get_ratings(top_show_data:list[dict])->list[dict]:
+    results = []
+    api_queries = []
+    base_query = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/"
+    wheel_of_time_query = f"{base_query}tt7462410"
+    api_queries.append(wheel_of_time_query)
+    first_query = f"{base_query}{top_show_data[0]['id']}"
+    api_queries.append(first_query)
+    fifty_query = f"{base_query}{top_show_data[49]['id']}"
+    api_queries.append(fifty_query)
+    hundred_query = f"{base_query}{top_show_data[99]['id']}"
+    api_queries.append(hundred_query)
+    two_hundered = f"{base_query}{top_show_data[199]['id']}"
+    api_queries.append(two_hundered)
+    for query in api_queries:
+        response = requests.get(query)
+        if response.status_code != 200:  # if we don't get an ok response we have trouble, skip it
+            print(f"Failed to get data, response code:{response.status_code} and error message: {response.reason} ")
+            continue
+        rating_data = response.json()
+        results.append(rating_data)
+    return results
+
+
+
 def main():
     top_show_data = get_top_250_data()
+    ratings_data = get_ratings(top_show_data)
+    report_results(ratings_data)
     report_results(top_show_data)
 
 
---
+++ readme.md
@@ -0,0 +1,2 @@
+# Instructors Project 1
+This is an instructors example of how one might do project 1 in the 2022 capstone projects. It currently contains sprint1
